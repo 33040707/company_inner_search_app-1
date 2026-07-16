19bfe3b6e0470d533cb6c34d7945e3e62c472624
Akira <piece.oo.clover8@gmail.com>
2026-07-16 14:13:01 +0900

    エラー詳細を表示

--- main.py
+++ main.py
@@ -44,6 +44,11 @@ except Exception as e:
     logger.error(f"{ct.INITIALIZE_ERROR_MESSAGE}\n{e}")
     # エラーメッセージの画面表示
     st.error(utils.build_error_message(ct.INITIALIZE_ERROR_MESSAGE), icon=ct.ERROR_ICON)
+
+    # ==========================================
+    # ★ここを追加：エラーの正体をブラウザ画面上に強制的に表示させる
+    # ==========================================
+    st.exception(e)
     # 後続の処理を中断
     st.stop()
 
